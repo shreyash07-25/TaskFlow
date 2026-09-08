b6d2e6a7966f56c8179a4aa429860a5df810c6f1
shreyash07-25 <sinhashreyash284@gmail.com>
2026-09-08 10:45:33 +0530

    Add Gunicorn and configure deployment port

--- backend/app.py
+++ backend/app.py
@@ -18,4 +18,8 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(debug=True, host="0.0.0.0", port=port)
